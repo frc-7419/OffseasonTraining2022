ff1462d0a3a50835dc54fa70c9f79b863f0505a0
wlmchen <wlmchen16@gmail.com>
2022-09-13 19:51:58 -0700

    pull from `programming-workshop` branch

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -15,6 +15,7 @@ import frc.robot.subsystems.autos.OneBallAuto;
 import frc.robot.subsystems.autos.OneBallAutoWait;
 import frc.robot.subsystems.autos.ThreeBallAutoExactVelocities;
 import frc.robot.subsystems.autos.ThreeBallAutoInterpolation;
+import frc.robot.subsystems.autos.TwoBallAutoDemo;
 import frc.robot.subsystems.autos.TwoBallAutoExactVelocities;
 import frc.robot.subsystems.autos.TwoBallAutoInterpolation;
 import frc.robot.subsystems.drive.ArcadeDrive;
@@ -29,9 +30,11 @@ import frc.robot.subsystems.intake.DeployIntakeWithJoystick;
 import frc.robot.subsystems.intake.IntakeSolenoidSubsystem;
 import frc.robot.subsystems.intake.IntakeSubsystem;
 import frc.robot.subsystems.led.LEDSubsystem;
+import frc.robot.subsystems.led.SetLEDColor;
 import frc.robot.subsystems.led.SetLEDColorWithJoystick;
 import frc.robot.subsystems.limelight.LimelightSubsystem;
 import frc.robot.subsystems.loader.LoaderSubsystem;
+import frc.robot.subsystems.shooter.GetToTargetVelocity;
 import frc.robot.subsystems.shooter.GetToTargetVelocityArbitraryFeedforward;
 import frc.robot.subsystems.shooter.GetToTargetVelocityWithLimelight;
 import frc.robot.subsystems.shooter.ShooterSubsystem;
@@ -62,14 +65,16 @@ public class RobotContainer {
   private final RunFeederWithJoystick runFeederWithJoystick = new RunFeederWithJoystick(feederSubsystem, joystick1);
   private final RunElevatorWithJoystick runElevatorWithJoystick = new RunElevatorWithJoystick(elevatorSubsystem, joystick2);
   private final RunArmsWithJoystick runArmsWithJoystick = new RunArmsWithJoystick(armsSubsystem, joystick2);
+  private final SetLEDColor setLEDColor = new SetLEDColor(ledSubsystem, limelightSubsystem, driveBaseSubsystem);
   private final SetLEDColorWithJoystick setLEDColorWithJoystick = new SetLEDColorWithJoystick(ledSubsystem, limelightSubsystem, driveBaseSubsystem, joystick1, joystick2);
   // private final SetLED1Color setled1Color = new SetLED1Color(ledSubsystem, driveBaseSubsystem);
   private final ArcadeDrive arcadeDrive = new ArcadeDrive(joystick1, driveBaseSubsystem, 0.95, 0.75);
 
   // auto
   // private SendableChooser<Command> autonChooser = new SendableChooser<>();
-  private final OneBallAuto oneBallAuto = new OneBallAuto(driveBaseSubsystem, gyroSubsystem, shooterSubsystem, limelightSubsystem, feederSubsystem, loaderSubsystem, ledSubsystem, turretSubsystem);
+  private final OneBallAuto oneBallAuto = new OneBallAuto(driveBaseSubsystem, gyroSubsystem, shooterSubsystem, limelightSubsystem, feederSubsystem, loaderSubsystem, ledSubsystem, turretSubsystem, intakeSubsystem, intakeSolenoidSubsystem);
   private final OneBallAutoWait oneBallAutoWait = new OneBallAutoWait(driveBaseSubsystem, gyroSubsystem, shooterSubsystem, limelightSubsystem, feederSubsystem, loaderSubsystem, ledSubsystem, turretSubsystem);
+  private final TwoBallAutoDemo twoBallAutoDemo = new TwoBallAutoDemo(driveBaseSubsystem, gyroSubsystem, shooterSubsystem, limelightSubsystem, feederSubsystem, loaderSubsystem, ledSubsystem, turretSubsystem, intakeSubsystem, intakeSolenoidSubsystem);
   private final TwoBallAutoExactVelocities twoBallAutoExactVelocities = new TwoBallAutoExactVelocities(driveBaseSubsystem, gyroSubsystem, shooterSubsystem, feederSubsystem, loaderSubsystem, intakeSubsystem, turretSubsystem, limelightSubsystem, ledSubsystem, intakeSolenoidSubsystem);
   private final TwoBallAutoInterpolation twoBallAutoInterpolation = new TwoBallAutoInterpolation(driveBaseSubsystem, gyroSubsystem, shooterSubsystem, feederSubsystem, loaderSubsystem, intakeSubsystem, turretSubsystem, limelightSubsystem, ledSubsystem, intakeSolenoidSubsystem);
   private final ThreeBallAutoExactVelocities threeBallAutoExactVelocities = new ThreeBallAutoExactVelocities(turretSubsystem, limelightSubsystem, shooterSubsystem, loaderSubsystem, feederSubsystem, driveBaseSubsystem, gyroSubsystem, intakeSubsystem, intakeSolenoidSubsystem, ledSubsystem);
@@ -82,35 +87,42 @@ public class RobotContainer {
   }
 
   private void configureButtonBindings() {
-    // align turret
-    new JoystickButton(joystick2, XboxController.Button.kLeftBumper.value)
-      .whileHeld(new AlignTurretDefault(turretSubsystem, limelightSubsystem));
-    
-    // brake turret during hang
-    new JoystickButton(joystick1, XboxController.Button.kB.value)
-      .whileHeld(new BrakeTurret(turretSubsystem));
-
-      // any distance, interpolation
+    // demos
     new JoystickButton(joystick2, XboxController.Button.kY.value)
-      .whileHeld(new GetToTargetVelocityWithLimelight(shooterSubsystem, limelightSubsystem));
+      .whileHeld(new GetToTargetVelocity(shooterSubsystem, 15, 15));
 
-    // edge of tarmac
     new JoystickButton(joystick2, XboxController.Button.kX.value)
-    .whileHeld(new GetToTargetVelocityArbitraryFeedforward(shooterSubsystem, 7900, 9900, 0.04874, 0.049));
+      .whileHeld(new GetToTargetVelocity(shooterSubsystem, 20, 20));
 
-    // lower hub shot, consistent
-    new JoystickButton(joystick2, XboxController.Button.kA.value)
-      .whileHeld(new GetToTargetVelocityArbitraryFeedforward(shooterSubsystem, 3500, 5450, 0.042, 0.0475));
+    // // align turret
+    // new JoystickButton(joystick2, XboxController.Button.kLeftBumper.value)
+    //   .whileHeld(new AlignTurretDefault(turretSubsystem, limelightSubsystem));
+    
+    // // brake turret during hang
+    // new JoystickButton(joystick1, XboxController.Button.kB.value)
+    //   .whileHeld(new BrakeTurret(turretSubsystem));
 
-      // coast arms during hanging
-    new JoystickButton(joystick2, XboxController.Button.kRightBumper.value)
-      .whileHeld(new CoastArms(armsSubsystem));
+    //   // any distance, interpolation
+    // new JoystickButton(joystick2, XboxController.Button.kY.value)
+    //   .whileHeld(new GetToTargetVelocityWithLimelight(shooterSubsystem, limelightSubsystem));
 
-    // toggle to maintain elevator position
-    new DoubleButton(
-      new JoystickButton(joystick2, XboxController.Button.kA.value),
-      new JoystickButton(joystick2, XboxController.Button.kB.value))
-      .toggleWhenPressed(new MaintainElevatorPosition(elevatorSubsystem));
+    // // edge of tarmac
+    // new JoystickButton(joystick2, XboxController.Button.kX.value)
+    // .whileHeld(new GetToTargetVelocityArbitraryFeedforward(shooterSubsystem, 7900, 9900, 0.04874, 0.049));
+
+    // // lower hub shot, consistent
+    // new JoystickButton(joystick2, XboxController.Button.kA.value)
+    //   .whileHeld(new GetToTargetVelocityArbitraryFeedforward(shooterSubsystem, 3500, 5450, 0.042, 0.0475));
+
+    //   // coast arms during hanging
+    // new JoystickButton(joystick2, XboxController.Button.kRightBumper.value)
+    //   .whileHeld(new CoastArms(armsSubsystem));
+
+    // // toggle to maintain elevator position
+    // new DoubleButton(
+    //   new JoystickButton(joystick2, XboxController.Button.kA.value),
+    //   new JoystickButton(joystick2, XboxController.Button.kB.value))
+    //   .toggleWhenPressed(new MaintainElevatorPosition(elevatorSubsystem));
   }
 
   private void smartDashboardBindings() {}
@@ -125,8 +137,9 @@ public class RobotContainer {
   }
 
   public Command getAutonomousCommand() {
-    return oneBallAutoWait;
-    // return oneBallAuto;
+    // return oneBallAutoWait;
+    return oneBallAuto;
+    // return twoBallAutoDemo;bottomTargetVelocity
     // return twoBallAutoExactVelocities;
     // return twoBallAutoInterpoloation;
     // return threeBallAutoExactVelocities;
@@ -145,7 +158,7 @@ public class RobotContainer {
     turretSubsystem.setDefaultCommand(runTurretWithJoystick);
     elevatorSubsystem.setDefaultCommand(runElevatorWithJoystick);
     armsSubsystem.setDefaultCommand(runArmsWithJoystick);
-    ledSubsystem.setDefaultCommand(setLEDColorWithJoystick);
+    ledSubsystem.setDefaultCommand(setLEDColor);
   }
 }
 
